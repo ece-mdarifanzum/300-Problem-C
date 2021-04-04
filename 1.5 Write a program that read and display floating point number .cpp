0864530a2caf7f7c099f9f42752c895602dd750a
MD. ARIF ANZUM <52672108+ece-mdarifanzum@users.noreply.github.com>
2021-04-05 02:13:22 +0600

    Add files via upload

---
+++ 1.5 Write a program that read and display floating point number .cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+
+    float n;
+    printf("Enter any floating point number: ");
+    scanf("%f",&n);
+    printf("%f",n);
+
+
+}
---
+++ 1.6 Write a program that read and display long number .cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    long n;
+    printf("Enter any longer number :");
+    scanf("%ld",&n);
+    printf("%ld",n);
+}
